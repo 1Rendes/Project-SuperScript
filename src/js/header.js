dd13d4b6172bdd7150c3b65546907e6d096d24ac
1Rendes <1admin@ukr.net>
2024-07-14 10:50:44 +0200

    mentor tips

--- src/js/header.js
+++ src/js/header.js
@@ -1,3 +1,5 @@
+import { body } from './work-together';
+
 const menuBtn = document.querySelector('.header-menubtn');
 const mobMenuBtn = document.querySelector('.header-mob-menubtn');
 const headerMenu = document.querySelector('.header-menu');
@@ -7,14 +9,17 @@ const headerCloseIcon = document.querySelector('.header-close-icon');
 menuBtn.addEventListener('click', () => {
   headerMenu.classList.toggle('header-hidden');
 });
-mobMenuBtn.addEventListener('click', () =>
-  headerMobMenu.classList.toggle('header-mob-hidden')
-);
-headerCloseIcon.addEventListener('click', () =>
-  headerMobMenu.classList.toggle('header-mob-hidden')
-);
+mobMenuBtn.addEventListener('click', () => {
+  body.style.overflow = 'hidden';
+  headerMobMenu.classList.toggle('header-mob-hidden');
+});
+headerCloseIcon.addEventListener('click', () => {
+  body.style.overflow = '';
+  headerMobMenu.classList.toggle('header-mob-hidden');
+});
 headerMobMenu.addEventListener('click', e => {
   if (e.target.nodeName === 'A') {
+    body.style.overflow = '';
     headerMobMenu.classList.toggle('header-mob-hidden');
   }
 });
